40760be7766b8783aa19f8a4824ca73ba463264d
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-08-21 16:46:03 -0700

    overall porject is not complete as far as the javascript but the site is funny so there is that.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,26 @@
 
-input = 15;
-let list=[];
 
-for (let i = 0; i <= input; i ++) {
-  list.push(i);
-}
+// let list=[];
 
-let result[]
-for (let i = list.length -1; num >0; i++) {
-  while ()
-}
+// for (let i = 0; i <= input; i ++) {
+//   list.push(i);
+// }
 
-function blopModifier(array, find){
-  let i = array.indexOf(find)
-  if (i == 3) {
-  list[i] = "Won't you be my neighbor?";
-   } else if (i == 2) {
-    list[i] = "Boop!";
-   } else if ( i == 1){
-    list[i] = "Beep!";
-   } else return find;
-  }
-
-$(document).ready(function() {
-  ("#input").submit(function(event) {
-    const nameInput = $("#name").val();
-    $(".result").show();
-    $(".page").hide();
-
-  });
+// function blopModifier(list, find){
+//   let i = list.indexOf(find)
+//   if (i == 3) {
+//   list[i] = "Won't you be my neighbor?";
+//    } else if (i == 2) {
+//     list[i] = "Boop!";
+//    } else if ( i == 1){
+//     list[i] = "Beep!";
+//    } else return find;
+//   }
+  $(document).ready(function() {
+    $(".btn").click(function() {
+      $(".result").show();
+      $(".jumbotron").hide();
+    });
   event.preventDefault();
 });
  
