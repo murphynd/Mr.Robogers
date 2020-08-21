cf8b7f4eca33cbb4e025f7e65560ecac60e65c7f
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-08-21 13:56:50 -0700

    accomplished finding the index of a specfic number to change to a string.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
 input = 15;
 let list=[];
-let stringlist=[];
-
 
 for (let i = 0; i <= input; i ++) {
-  list.push(i.toString());
+  list.push(i);
+}
+
+let index= list.indexOf(3)
+if (index !== -1) {
+list[index] = "BOO";
 }
