1334ba2eed2a781431822fd4a71cbae703782122
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-08-21 11:01:32 -0700

    Stummped trying to solve to get a number to replace in the array

--- README.md
+++ README.md
@@ -28,9 +28,9 @@ Input: "4"
 Output: "0, 1, 2, 3, 4"
 Include similar specifications in your README for all behaviors your program demonstrates.
 
-Spec: The program returns numbers that start with 1 from new list
+Spec: The program returns numbers that start with 3 from new list
 Input: "4"
-Output: "1"
+Output: "3"
 
 Spec: The program returns a range of numbers from 0 to the users inputted number
 Input: "4"
--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,14 @@ for (let i = 0; i <= input; i ++) {
   list.push(i);
 }
 console.log(list);
+
+const index = list.indexOf(input);
+if (i> -1) {
+  array.splice(i, 1);
+}
 list.forEach(function(i){
 
-if (list.includes(3)){
+if (list.includes(3).unshift(3)){
 list.unshift("Won't you be my neighbor?");
 }
 else if(list.includes(2)){
@@ -19,3 +24,16 @@ list.unshift("beep")
 });
 console.log(xArray)
 console.log(list)
+
+input = 15;
+let list=[];
+for (let i = 0; i <= input; i ++) {
+  list.push(i);
+}
+console.log(list);
+
+for (let i =0; list.length; i++)
+	if (list[i] == 1)
+	list[i]="beep";
+
+console.log(i);
