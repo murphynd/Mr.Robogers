b7bcc59cfe772ccb38d387a05916ae1586584649
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-08-21 14:33:11 -0700

    working out formula for these three actions to run

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,18 @@ for (let i = 0; i <= input; i ++) {
   list.push(i);
 }
 
-let index= list.indexOf(3)
+let index3= list.indexOf(3)
 if (index !== -1) {
-list[index] = "BOO";
+list[index] = "Won't you be my neighbor?";
+}
+console.log(list)
+
+let index2= list.indexOf(2)
+if (index !== -1) {
+list[index] = "Boop!";
+}
+
+let index1= list.indexOf(1)
+if (index !== -1) {
+list[index] = "Beep!";
 }
