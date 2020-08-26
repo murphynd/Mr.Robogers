3219ca3607aa66cc718b7e4ae7aa01d1b417a8dd
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-08-25 17:24:20 -0700

    will make javaScript local and removes business logic. updates HTML to plane version. IT WORKS

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,25 @@
-
-function makearray(input) {
-  this.array = []
-  for (let i = 0; i <= input; i ++)  {
-    this.array.push(i);
-  }
-}
-function blopModifierlist() {
-  let result = []
-  for(let i = 0; i < this.array.length; i++) {
-    if (i.toString().includes('3')) {
-    result.push("Won't you be my neighbor?");
-    } else if (i.toString().includes('2')){
-    result.push = ("Boop!");
-    } else if (i.toString().includes('1')){
-    result.push = ("Beep!");
-    } else 
-      result.push(i);
-    }
-  return result;
-}
-
-$(document).ready(function() {
-  $(".btn").click(function() {
-    $(".result").show();
-    $(".jumbotron").hide();
-  });
-  
-  $("form#input").submit(function(event) {
+$(document).ready(function () {
+  $("form#input").submit(function (event) {
     event.preventDefault();
-    let userNumber = makearray(parseInt($("#inputnum").val()));
-    console.log(userNumber);
-    blopModifierlist(userNumber);
-    console.log(userNumber);
+    let userNumber = $("#inputnum").val();
+    let result = [];
+    let array = [];
+
+    for (let i = 0; i <= userNumber; i++) {
+      array.push(" " + i);
+    }
+
+    array.forEach(function (element) {
+      if (element.includes(3)) {
+        result.push("Won't you be my neighbor?");
+      } else if (element.includes(2)) {
+        result.push("Boop!");
+      } else if (element.includes(1)) {
+        result.push("Beep!");
+      } else {
+        result.push(element);
+      }
+    });
+    $("#blop").val(result);
   });
 });
