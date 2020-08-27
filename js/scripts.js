c413132c433fa7579ebe7fefee631d5c3d68f579
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-08-27 08:15:53 -0700

    moved functions into business logic to streamline User Logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,32 @@
+function makeArray(userNumberparam) {
+  let array = [];
+  for (let i = 0; i <= userNumberparam; i++) {
+    array.push(" " + i);
+  }
+  return array;
+}
+function blopModifier(list) {
+  let result = [];
+  list.forEach(function (element) {
+    if (element.includes(3)) {
+      result.push("Won't you be my neighbor?");
+    } else if (element.includes(2)) {
+      result.push("Boop!");
+    } else if (element.includes(1)) {
+      result.push("Beep!");
+    } else {
+      result.push(element);
+    }
+  });
+  return result;
+}
+
 $(document).ready(function () {
   $("form#input").submit(function (event) {
     event.preventDefault();
     let userNumber = $("#inputnum").val();
-    let result = [];
-    let array = [];
-
-    for (let i = 0; i <= userNumber; i++) {
-      array.push(" " + i);
-    }
-
-    array.forEach(function (element) {
-      if (element.includes(3)) {
-        result.push("Won't you be my neighbor?");
-      } else if (element.includes(2)) {
-        result.push("Boop!");
-      } else if (element.includes(1)) {
-        result.push("Beep!");
-      } else {
-        result.push(element);
-      }
-    });
-    $("#blop").val(result);
+    let newArray = makeArray(userNumber);
+    let newResult = blopModifier(newArray);
+    $("#blop").val(newResult);
   });
 });
